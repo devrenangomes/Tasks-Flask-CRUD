424d6844a87cff65b17a61b8b9b9d553414b2bfa
devrenangomes <renandaluzgomes@gmail.com>
2026-03-13 23:42:34 -0300

    feat: Add GET endpoints to retrieve all tasks and a single task, and rename the task list variable.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from models.task import Task
 
 app = Flask(__name__)
 
-task = []
+tasks = []
 task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
@@ -12,9 +12,29 @@ def create_task():
     data = request.get_json()
     new_task = Task(id = task_id_control, title = data.get('title'), description = data.get('description'))
     task_id_control += 1
-    task.append(new_task)
-    print(task)
+    tasks.append(new_task)
+    print(tasks)
     return jsonify({'message': 'Nova tarefa criada com sucesso'})
+
+
+@app.route('/tasks', methods = ['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+                'tasks': task_list,
+                'total_tasks': len(task_list)
+    }
+    return jsonify(output)
+
+
+@app.route('/tasks/<int:id>', methods = ['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+            
+    return jsonify({'message': 'Não foi possível encontrar a atividade.'}), 404
 
 if __name__ == "__main__":
     app.run(debug = True)
